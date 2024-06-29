9925db82cb3d85a5685f5e749fe7734335095347
zxc <794350035@qq.com>
2024-06-29 21:26:06 +0800

    Replace the markdown editor and add gpt output tab and prompt options

--- chrome-ext/sitestash/src/contentScript/Content.vue
+++ chrome-ext/sitestash/src/contentScript/Content.vue
@@ -1,17 +1,29 @@
 <script setup lang="js">
-import {Readability} from '@mozilla/readability'
 import TurndownService from "turndown"
-import {ref, onMounted} from 'vue'
-import {ElMessageBox} from 'element-plus'
-import {MdEditor} from 'md-editor-v3';
-import 'md-editor-v3/lib/style.css';
+import {ref, onMounted, reactive, nextTick, watch, watchEffect} from 'vue'
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 import {ElNotification} from 'element-plus'
-
 const dialogVisible = ref(false)
 const isSelecting = ref(false)
 const title = ref("")
 const article = ref("hello world")
+const toastuiEditor = ref(null)
+let editor
+watchEffect(() => {
+  if (toastuiEditor.value) {
+    console.log(toastuiEditor.value)
+    console.log(article.value)
+    editor = new Editor({
+      el: toastuiEditor.value,
+      previewStyle: 'vertical',
+      height: '500px',
+      initialValue: article.value
+    })
+  }
+})
 onMounted(() => {
+
   let hoveredElement = null;
   title.value = document.title
 
@@ -50,7 +62,7 @@ onMounted(() => {
       }
       isSelecting.value = true;
       ElNotification({
-        title: 'Success',
+        title: 'Site Stash',
         message: 'Move your mouse to select text area',
         type: 'success',
       })
@@ -67,13 +79,50 @@ onMounted(() => {
     if (request.action === "getPageTitle") {
       isSelecting.value = true;
       ElNotification({
-        title: 'Success',
+        title: 'Site Stash',
         message: 'Move your mouse to select text area',
         type: 'success',
       })
     }
   });
 })
+const generateContent = () => {
+
+}
+
+const settings = () => {
+
+}
+
+const forms = reactive({
+  prompt: ""
+})
+const dynamicTags = ref(['Tag 1', 'Tag 2', 'Tag 3'])
+const handleClose = (tag) => {
+  dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1)
+}
+const inputVisible = ref(false)
+const InputRef = ref(null)
+const inputValue = ref('')
+const showInput = () => {
+  inputVisible.value = true
+  nextTick(() => {
+    InputRef.value.input.focus()
+  })
+}
+const handleInputConfirm = () => {
+  if (inputValue.value) {
+    dynamicTags.value.push(inputValue.value)
+  }
+  inputVisible.value = false
+  inputValue.value = ''
+}
+watch(article, (newValue, oldValue) => {
+  console.log(`watch: count changed from ${oldValue} to ${newValue}`);
+  if (editor) {
+    editor.setMarkdown(newValue)
+  }
+});
 </script>
 
 <template>
@@ -83,9 +132,59 @@ onMounted(() => {
       :fullscreen="true"
       destroy-on-close
   >
-    <el-scrollbar>
-      <MdEditor v-model="article"/>
-    </el-scrollbar>
+    <div style="height: 85vh;">
+
+
+      <el-form :inline="true" :model="forms">
+        <el-form-item label="Prompt Select">
+          <el-select
+              v-model="forms.prompt"
+              placeholder="Prompt Select"
+              clearable
+          >
+            <el-option label="Zone one" value="shanghai"/>
+            <el-option label="Zone two" value="beijing"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="generateContent">Generate Content</el-button>
+          <el-button @click="settings">Settings</el-button>
+        </el-form-item>
+      </el-form>
+      <div class="flex gap-2">
+        KeyWords:
+        <el-tag
+            v-for="tag in dynamicTags"
+            :key="tag"
+            closable
+            :disable-transitions="false"
+            @close="handleClose(tag)"
+        >
+          {{ tag }}
+        </el-tag>
+        <el-input
+            v-if="inputVisible"
+            ref="InputRef"
+            v-model="inputValue"
+            class="w-20"
+            size="small"
+            @keyup.enter="handleInputConfirm"
+            @blur="handleInputConfirm"
+        />
+        <el-button v-else class="button-new-tag" size="small" @click="showInput">
+          + New Tag
+        </el-button>
+      </div>
+      <el-tabs type="border-card" style="margin-top: 20px">
+        <el-tab-pane label="GPT Output">
+          GPT Output
+        </el-tab-pane>
+        <el-tab-pane label="Origin Content">
+          <div ref="toastuiEditor"></div>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+
 
     <template #footer>
       <div class="dialog-footer">
@@ -99,5 +198,7 @@ onMounted(() => {
 </template>
 
 <style scoped>
-
+.el-select {
+  --el-select-width: 220px;
+}
 </style>
